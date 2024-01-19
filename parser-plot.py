1e349a406068f6926f397063c8593efbbdba266b
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2024-01-19 17:53:44 +0800

    Update parser-plot.py

--- parser-plot.py
+++ parser-plot.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import math
 
+#check_all_infinite函数用于检查给定数据集中的所有元素是否为无穷大，如果是，则返回True，否则返回False。
 def check_all_infinite(dataset):
   for el in dataset:
     if not math.isinf(el):
       return False
   return True
 
+#plot_data函数用于绘制数据，它接受一个数据集和一个名称作为参数，并创建一个图形对象和一个坐标轴对象。然后，它遍历数据集中的选定列，并将其绘制在图形上。
 def plot_data(dataset, name="rsu"):
   fig, ax = plt.subplots(figsize=(15, 15))
   #for i in range(1, len(data.columns)):
@@ -34,7 +36,8 @@ plot_data(data, "RSU_8")
 data = pd.read_csv("powerTest-10.txt",sep="\t", header= None)
 plot_data(data, "RSU_10")
 
-
+#最后，代码读取了一个名为losCondition-3.txt的数据文件，并使用plot_data函数绘制了所有列的数据。
+#它创建了一个图形对象和一个坐标轴对象，并将所有数据绘制在同一图形上。然后，它设置了图形的标题、坐标轴标签和图例，并显示了图形。
 data = pd.read_csv("losCondition-3.txt",sep="\t", header= None)
 fig, ax = plt.subplots(figsize=(15, 15))
 #for i in range(1, len(data.columns)):
