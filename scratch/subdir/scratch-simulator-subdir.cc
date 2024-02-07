073f9b86cc7a6e05bd16796dc6a555f8e54e85d7
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2024-02-07 18:43:52 +0800

    Update scratch-simulator-subdir.cc

--- scratch/subdir/scratch-simulator-subdir.cc
+++ scratch/subdir/scratch-simulator-subdir.cc
@@ -27,3 +27,16 @@ main (int argc, char *argv[])
   CommandLine cmd;
   cmd.Parse (argc, argv);
 }
+
+
+
+/*
+#include "ns3/core-module.h"：包含了NS-3核心模块的头文件，这些模块提供了网络模拟器的基本功能。
+using namespace ns3;：使用了NS-3命名空间，这样可以直接访问NS-3库中的类和函数，而无需每次都指定命名空间。
+NS_LOG_COMPONENT_DEFINE ("ScratchSimulator");：定义了日志组件名称为"ScratchSimulator"，用于在NS-3中输出日志。
+int main(int argc, char *argv[])：程序的入口函数，接受命令行参数作为输入。
+NS_LOG_UNCOND ("Scratch Simulator");：使用NS-3的日志系统输出一条日志消息，表示程序开始执行。
+CommandLine cmd;：创建了一个命令行解析器对象，用于解析命令行参数。
+cmd.Parse(argc, argv);：解析命令行参数，使得在程序运行时可以传入参数。
+该程序本身并未执行具体的网络模拟任务，而是作为一个NS-3模拟器的入口，初始化了日志系统，并提供了命令行参数的解析功能，以便于后续的网络模拟任务的执行和参数设置。
+*/
